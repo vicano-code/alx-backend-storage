ca3ff1f22f98dd80c1c2bfe1eea0a80c7963ae0c
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-08-08 11:29:57 +0000

    sql statement correction

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,7 +6,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE avg_score FLOAT;
-	SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
-	UPDATE users SET average_score = avg_score WHERE id = user_id;
+	SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+	UPDATE users SET average_score = avg_score WHERE id=user_id;
 END //
 DELIMITER ;
